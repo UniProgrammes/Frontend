c32338b3921a2a71c1c1b83e314cf88a777244cd
Carlos Gálvez <c.galvezmena@gmail.com>
2024-11-02 20:37:56 +0100

    feat: created the StudyPlanCard component

---
+++ src/components/StudyPlanCard.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+interface StudyPlanCardProps {
+    name: string,
+    completed: string;
+    numCourses: string;
+    lastUpdate: string;
+  }
+
+  const StudyPlanCard: React.FC<StudyPlanCardProps> = ({ name, completed, numCourses, lastUpdate }) => {
+  return (
+    <div className="bg-white shadow-md rounded-3xl p-4 w-fit">
+      <h3 className="text-lg font-bold text-white p-4 my-2 rounded-2xl bg-purple-500 text-center">{name}</h3>
+      <p className="m-2 mt-8">{completed ? "Completed" : "Not Completed"}</p>
+      <p className="m-2"> {numCourses} courses selected</p>
+      <p className="m-2 mb-8">Last Updated on: {lastUpdate}</p>
+      <button className="px-4 py-2 bg-purple-500 text-white rounded-full text-center w-full">Modify Study Plan</button>
+    </div>
+  )
+}
+
+export default StudyPlanCard;
