718aba9f0b0cbc8b62ec6fccae1df6f46092f616
Carlos Gálvez <c.galvezmena@gmail.com>
2024-11-25 00:06:26 +0100

    fix: changing link using navigate

--- src/components/StudyPlanCard.tsx
+++ src/components/StudyPlanCard.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { useNavigate } from "react-router-dom";
+
 interface StudyPlanCardProps {
     id: string,
     name: string,
@@ -8,18 +10,21 @@ interface StudyPlanCardProps {
     lastUpdate: string;
 }
 
-  const StudyPlanCard: React.FC<StudyPlanCardProps> = ({ id, name, completed, numCourses, lastUpdate }) => {
+const StudyPlanCard: React.FC<StudyPlanCardProps> = ({ id, name, completed, numCourses, lastUpdate }) => {
+  
+  const navigate = useNavigate();
+
   return (
     <div id={id} className="bg-white shadow-md rounded-3xl p-4 w-fit">
       <h3 className="text-lg font-bold text-white p-4 my-2 rounded-2xl bg-purple-500 text-center">{name}</h3>
       <p className="m-2 mt-8">{completed ? "" : "Not"} Completed</p>
       <p className="m-2"> {numCourses} courses selected</p>
       <p className="m-2 mb-8">Last Updated on: {lastUpdate}</p>
-      <a href="http://localhost:3000/study-plan">
-        <button className="px-4 py-2 bg-purple-500 hover:bg-purple-700 text-white rounded-full text-center w-full">
-          {completed ? "View" : "Modify"} Study Plan
-        </button>
-      </a>
+      <button 
+        onClick={() => {navigate(`/study-plan/${id}`)}}
+        className="px-4 py-2 bg-purple-500 hover:bg-purple-700 text-white rounded-full text-center w-full">
+        {completed ? "View" : "Modify"} Study Plan
+      </button>
     </div>
   )
 }
