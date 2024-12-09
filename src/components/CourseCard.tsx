2ce6fa36eff754da740f29852bc3ab22d5f9f35e
Carlos Gálvez <c.galvezmena@gmail.com>
2024-12-09 10:54:36 +0100

    feat: added semester info

--- src/components/CourseCard.tsx
+++ src/components/CourseCard.tsx
@@ -6,6 +6,7 @@ interface CourseCardProps {
   code: string;
   educational_level: string;
   description: string;
+  semester: number;
   onRemove: () => void;
 }
 
@@ -15,16 +16,18 @@ const CourseCard: React.FC<CourseCardProps> = ({
   code,
   educational_level,
   description,
+  semester,
   onRemove
 }) => {
   return (
     <div className="bg-white shadow-md rounded-3xl p-4 w-full">
         <div id="current-plan" className="flex flex-row items-center justify-between px-4 py-2">
             <h3 className="text-2xl font-bold text-black m-2 mb-4 text-left">{name}</h3>
-            <button className="block text-xl w-1/3 m-4 h-10 text-left p-2 rounded-lg text-white text-center text-bold bg-purple-600">
+            <button className="block text-xl w-1/3 m-4 h-10 p-2 rounded-lg text-white text-center text-bold bg-purple-600">
                 Programme
             </button>
         </div>
+        <p className="m-2 pl-4">Semester: {semester}</p>
         <p className="m-2 pl-4">{credits} ECTS credits</p>
         <p className="m-2 pl-4">{educational_level}</p>
         <p className="m-2 pl-4">{description}</p>
